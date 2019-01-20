ce2887dde36ced08ffb100eb2c615e1253f44b27
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-20 07:37:12 -0500

    bundled gems and created routes

---
+++ controller.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+
+get '/' do
+
+end
+
+get '/signin' do
+
+end
+
+post '/signin' do
+
+end
+
+get '/signup' do
+
+end
+
+post '/signup' do
+
+end
+
+get '/dashboard' do
+
+end
+
+post '/dashboard' do
+
+end
+
+post '/signout' do
+
+end
+
+get '/profile' do
+
+end
+
+post '/deleteuser' do
+
+end
