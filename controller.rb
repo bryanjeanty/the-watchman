b326a3ecb78325a936b5f0a29067875f5163f728
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-20 07:43:46 -0500

    Added templates to routes and created redirects

--- controller.rb
+++ controller.rb
@@ -1,41 +1,41 @@
 require 'sinatra'
 
 get '/' do
-
+    erb(:index)
 end
 
 get '/signin' do
-
+    erb(:signin)
 end
 
 post '/signin' do
-
+    redirect '/dashboard'
 end
 
 get '/signup' do
-
+    erb(:signup)
 end
 
 post '/signup' do
-
+    redirect '/signin'
 end
 
 get '/dashboard' do
-
+    erb(:dashboard)
 end
 
 post '/dashboard' do
-
+    redirect '/dashboard'
 end
 
 post '/signout' do
-
+    redirect '/'
 end
 
 get '/profile' do
-
+    erb(:profile)
 end
 
 post '/deleteuser' do
-
+    redirect '/'
 end
