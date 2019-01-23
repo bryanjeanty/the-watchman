bd13f5be218aabb4f1cb52795dd8e6fc7d67868e
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-23 11:43:44 -0500

    modified posts section

---
+++ public/scripts/events.js
@@ -0,0 +1,8 @@
+// let down_btn = document.querySelector('.down-btn');
+// let count = 1;
+
+// down_btn.addEventListener('click', (event) => {
+//     let post_page = document.querySelector(`.posts-${ count }`);
+//     post_page.style.display = "block";
+//     count += 1;
+// });
