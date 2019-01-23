890ba448017d831fc7fd7ad016dfadfee7631b02
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-01-23 14:27:26 -0500

    Added seed data to web app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,28 @@
-require_relative '../controller'
 require 'csv'
+require_relative '../models'
+require_relative '../avatar'
 
-# CSV.foreach('./USR_DATA.csv', headers: true) do |row|
-#     User.create(
-#         username: row['username'],
-#         password: row['password']
-#     )
-# end
+CSV.foreach(File.join(Dir.pwd,'/db/USR_DATA.csv'), headers: true) do |row|
+    avatar = Avatar.new(row['first_name'], row['last_name'])
+    avatar_url = avatar.get_avatar_url
+    User.create(
+        id: row['id'],
+        username: row['username'],
+        password: row['password'],
+        first_name: row['first_name'],
+        last_name: row['last_name'],
+        email: row['email'],
+        birthday: row['birthday'],
+        updated: Time.now,
+        avatar_url: avatar_url
+    )
+end
 
-User.create(
-    username: 'bryanjay',
-    password: 'jay_bryan01'
-)
+CSV.foreach(File.join(Dir.pwd,'/db/POST_DATA.csv'), headers: true) do |row|
+    Post.create(
+    title: row['title'],
+    content: row['content'],
+    user_id: row['user_id'],
+    updated: Time.now
+    )
+end
